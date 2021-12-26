bcba42317248ad4d898cd4a298c738b2e414f957
shiv <s.shohei.engineer@gmail.com>
2021-12-26 19:17:55 +0900

    first commit

---
+++ 100/1.py
@@ -0,0 +1,21 @@
+
+
+while True:
+
+    li = list(map(int, input().split()))
+    n = li[0]
+    x = li[1]
+
+    if x == 0 and n == 0:
+        break
+
+    ans = 0
+    for num1 in range(1, n+1):
+        for num2 in range(num1+1, n+1):
+            for num3 in range(num2+1, n+1):
+                if (num1 + num2 + num3) == x:
+                    ans += 1
+
+    print(ans)
+
+
