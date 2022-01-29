4a8ef92f8e9e5f4b28c9dfb3d113b0ab873f2d0c
shiv <s.shohei.engineer@gmail.com>
2022-01-29 22:09:49 +0900

    wf

--- Argorithm/reference.py
+++ Argorithm/reference.py
@@ -1,7 +1,11 @@
-###標準入力
-##1 2 >>> x=1, y=2
-x, y = map(int, input().split())
+#標準出力
+    #改行無し
+        print(text, end="")
+#標準入力
+    1 2 >>> x=1, y=2
+        x, y = map(int, input().split())
 
-#1 2 3 >>> [1,2,3]
-li = list(map(int, input().split()))
+    #1 2 3 >>> [1,2,3]
+       li = list(map(int, input().split()))
 
+#配列
