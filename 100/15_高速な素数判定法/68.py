cca0f8071fc63f7929ccb5643d2b645b1f686d78
shohei <s.shohei.engineer@gmail.com>
2023-03-26 14:54:40 +0900

    68

--- 100/15_高速な素数判定法/68.py
+++ 100/15_高速な素数判定法/68.py
@@ -1,2 +1,30 @@
 #https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
 
+import math
+
+def factorization(n):
+    pfs = []
+    while n >= 2:
+        pf = first_prime(n)
+        if pf != n:
+            pfs.append(pf)
+            n = n / pf
+        else:
+            pfs.append(n)
+            break
+    return pfs
+
+def first_prime(n):
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return i
+    return int(n)
+
+
+n_ipt = int(input())
+
+print(n_ipt, end=":")
+for i in factorization(n_ipt):
+    print(" ", end="")
+    print(int(i), end="")
+print()
